e0a98fbf0bc3c2ca0a5e67bbdf97f9f4daec3c22
Márcio Costa <marcio.costa@virtus.ufcg.edu.br>
2022-07-12 22:41:19 -0300

    Home component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./components/templates/header/header.component";
 import { FooterComponent } from "./components/templates/footer/footer.component";
 import { NavComponent } from "./components/templates/nav/nav.component";
+import { HomeComponent } from "./components/views/home/home.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    HeaderComponent, 
-    FooterComponent, 
-    NavComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,6 +29,7 @@ import { MatIconModule } from "@angular/material/icon";
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
