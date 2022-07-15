75becf4f8b8510f9260020a114478b093b21309e
Márcio Costa <marcio.costa@virtus.ufcg.edu.br>
2022-07-14 22:13:07 -0300

    Read categories implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -7,11 +8,13 @@ import { HeaderComponent } from "./components/templates/header/header.component"
 import { FooterComponent } from "./components/templates/footer/footer.component";
 import { NavComponent } from "./components/templates/nav/nav.component";
 import { HomeComponent } from "./components/views/home/home.component";
+import { CategoryReadComponent } from "./components/views/category/category-read/category-read.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [
@@ -20,16 +23,19 @@ import { MatCardModule } from "@angular/material/card";
     FooterComponent,
     NavComponent,
     HomeComponent,
+    CategoryReadComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent],
